2ea5537ceae3654a236c7f32c177d6b8f496566a
Martino Thomann <martino.thomann@gmail.com>
2026-03-29 20:50:00 +0200

    [nvim] - set oil highlight group

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -22,5 +22,6 @@ return {
         })
         vim.api.nvim_set_hl(0, "OilDirHidden", { link = "OilDir" })
         vim.api.nvim_set_hl(0, "OilFileHidden", { link = "OilFile" })
+        vim.api.nvim_set_hl(0, "OilEmpty", { link = "OilFile" })
     end
 }
